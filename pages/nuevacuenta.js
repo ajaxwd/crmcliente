445c227752fb42c5d1512bbcd188e44d693bccd2
Adrián Siñiga Meléndez <adrian.siniga@bicevida.cl>
2020-10-30 18:48:00 -0300

    Se configura apollo client

--- pages/nuevacuenta.js
+++ pages/nuevacuenta.js
@@ -14,7 +14,16 @@ const NuevaCuenta = () => {
             password: ''
         },
         validationSchema: Yup.object({
-            nombre: Yup.string().required('El nombre es obligatorio')
+            nombre: Yup.string()
+                        .required('El nombre es obligatorio'),
+            apellido: Yup.string()
+                        .required('El password es obligatorio'),
+            email: Yup.string()
+                        .email('El email no es valido')
+                        .required('El email es obligatorio'),
+            password: Yup.string()
+                        .required('El password no puede ir vacio')
+                        .min(6, 'El password deber ser de al menos 6 caracteres')
         }),
         onSubmit: valores => {
             console.log('');
@@ -63,6 +72,13 @@ const NuevaCuenta = () => {
                         onChange = {formik.handleChange}>
                         </input>
                     </div>
+                    { formik.touched.apellido && formik.errors.apellido ? (
+                        <div className = "my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                            <p className = "font-bold">Error</p>
+                            <p>{formik.errors.apellido}</p>
+                        </div>
+                        ) : null
+                    }
                     <div className = "mb-4"> 
                         <label className = "block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
                         Email
@@ -76,6 +92,13 @@ const NuevaCuenta = () => {
                         onChange = {formik.handleChange}>
                         </input>
                     </div>
+                    { formik.touched.email && formik.errors.email ? (
+                        <div className = "my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                            <p className = "font-bold">Error</p>
+                            <p>{formik.errors.email}</p>
+                        </div>
+                        ) : null
+                    }
                     <div className = "mb-4">
                         <label className = "block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                         Password
@@ -89,6 +112,13 @@ const NuevaCuenta = () => {
                         onChange = {formik.handleChange}>
                         </input>
                     </div>
+                    { formik.touched.password && formik.errors.password ? (
+                        <div className = "my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                            <p className = "font-bold">Error</p>
+                            <p>{formik.errors.password}</p>
+                        </div>
+                        ) : null
+                    }
                     <input 
                     type = "submit"
                     className = "bg-gray-800 w-full mt-5 p-2 text-white uppercase hover:bg-gray-900"
