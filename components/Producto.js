4a7c5f1ded1099f147e634013ea6c61d18f530cd
Adrián Siñiga Meléndez <adrian.siniga@bicevida.cl>
2020-11-09 12:57:03 -0300

    Se da inicio al formulario de productos

---
+++ components/Producto.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Producto = ({}) => {
+    const { nombre } = produto;
+
+    return (
+        <h1>{nombre}</h1>
+    )
+}
+
+export default Producto;
--- pages/productos.js
+++ pages/productos.js
@@ -1,10 +1,46 @@
 import React from 'react';
 import Layout from '../components/layout';
+import { gql, useQuery} from '@apollo/client';
+
+const OBTENER_PRODUCTOS = gql`
+    query obtenerProductos {
+        obtenerProducots {
+            id 
+            nombre
+            precio
+            existencia
+        }
+    }
+`;
 
 const Productos = () => {
+
+    //Consultar productos
+    const {data, loading, error} = useQuery(OBTENER_PRODUCTOS);
+
     return (
         <Layout>
-            <h1>Productos</h1>
+            <h1 className = "text-2xl text-gray-800 font-light">Productos</h1>
+
+            <table className = 'table-auto shadow-md mt-10 w-full w-lg'>
+        <thead className = 'bg-gray-800'>
+          <tr className = 'text-white'>
+            <th className = 'w-1/5 py-2'>Nombre</th>
+            <th className = 'w-1/5 py-2'>Existencia</th>
+            <th className = 'w-1/5 py-2'>Precio</th>
+            <th className = 'w-1/5 py-2'>Eliminar</th>
+            <th className = 'w-1/5 py-2'>Editar</th>
+          </tr>
+        <tbody>
+          {data.obtenerProducto.map( producto => (
+            <Producto
+            key = {producto.id}
+            producto = {producto}
+            />
+          ))}
+        </tbody>
+        </thead>
+      </table>
         </Layout>
     );
 }
