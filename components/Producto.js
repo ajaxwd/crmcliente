208ade26c75614e95de0814d9fa1f28ac034a73a
Adrián Siñiga Meléndez <adrian.siniga@bicevida.cl>
2020-11-30 23:23:03 -0300

    formulario de edicion de productos

--- components/Producto.js
+++ components/Producto.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Swal from 'sweetalert2';
 import {gql, useMutation} from '@apollo/client';
+import Router from 'next/router';
 
 const ELIMINAR_PRODUCTO = gql`
     mutation eliminarProducto($id: ID!){
@@ -71,6 +72,13 @@ const Producto = ({producto}) => {
         })
     }
 
+    const editarProducto = () => {
+        Router.push({
+            pathname: "/editarproducto/[is]",
+            query: {id}
+        })
+    }
+
     return (
         <tr>
             <td className = "border px-4 py-2">{nombre}</td>
@@ -89,7 +97,7 @@ const Producto = ({producto}) => {
             <button
                     type = "button"
                     className = "flex justify-center items-center bg-green-600 py-2 px-4 w-full text-white rounded text-xs uppercase font-bold"
-                    //onClick = {() => editarCliente()}
+                    onClick = {() => editarProducto()}
                 >
                     Editar
                 </button>
---
+++ pages/editarproducto/[pid].js
@@ -0,0 +1,120 @@
+import React from 'react';
+import Layout from '../../components/layout';
+import {useRouter} from 'next/router';
+import {gql, useQuery} from '@apollo/client';
+
+const OBTENER_PRODUCTO = gql`
+    query obtenerProducto($id: ID!) {
+        obtenerProducot(id: $id) {
+            nombre
+            precio
+            existencia
+        }
+    }
+`;
+
+const EditarProducto = () => {
+    const router = useRouter();
+    const {query: {id}} = router;
+
+    //Consultar para obtener el producto
+    const {data, loading, error} = useQuery(OBTENER_PRODUCTO, {
+        variables: {
+            id
+        }
+    });
+
+    if(loading) return 'Cargando...';
+
+    const {obtenerProducto} = data;
+
+    return (
+        <Layout>
+            <h1 className = "text-2xl text-gray-800 font-light">Editar Producto</h1>
+
+            <div className = "flex justity-center mt-5">
+                <div className = "w-full max-w-lg">
+                    <form className = "bg-white shadow-md px-8 pt-6 pb-8 mb-4"
+                    //onsubmit = {formik.handleSubmit}
+                    >
+                    <div className = "mb-4"> 
+                        <label className = "block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">
+                        Nombre
+                        </label>
+                        <input 
+                        className = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline focus:shadow-outline"
+                        id = "nombre"
+                        type = "text"
+                        placeholder = "Nuevo Cliente"
+                        value = {formik.values.nombre}
+                        onChange = {formik.handleChange}
+                        onBlur = {formik.handleBlur}
+                        >                      
+                        </input>
+                    </div>
+                    { formik.touched.nombre && formik.errors.nombre ? (
+                        <div className = "my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                            <p className = "font-bold">Error</p>
+                            <p>{formik.errors.nombre}</p>
+                        </div>
+                        ) : null
+                    }
+                    <div className = "mb-4"> 
+                        <label className = "block text-gray-700 text-sm font-bold mb-2" htmlFor="existencia">
+                        Cantidad Disponible
+                        </label>
+                        <input 
+                        className = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline focus:shadow-outline"
+                        id = "existencia"
+                        type = "number"
+                        placeholder = "Cantidad Disponible"
+                        value = {formik.values.existencia}
+                        onChange = {formik.handleChange}
+                        onBlur = {formik.handleBlur}
+                        >                      
+                        </input>
+                    </div>
+                    { formik.touched.existencia && formik.errors.existencia ? (
+                        <div className = "my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                            <p className = "font-bold">Error</p>
+                            <p>{formik.errors.existencia}</p>
+                        </div>
+                        ) : null
+                    }
+                    <div className = "mb-4"> 
+                        <label className = "block text-gray-700 text-sm font-bold mb-2" htmlFor="precio">
+                        Precio
+                        </label>
+                        <input 
+                        className = "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline focus:shadow-outline"
+                        id = "precio"
+                        type = "number"
+                        placeholder = "Precio Producto"
+                        value = {formik.values.precio}
+                        onChange = {formik.handleChange}
+                        onBlur = {formik.handleBlur}
+                        >                      
+                        </input>
+                    </div>
+                    { formik.touched.precio && formik.errors.precio ? (
+                        <div className = "my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                            <p className = "font-bold">Error</p>
+                            <p>{formik.errors.precio}</p>
+                        </div>
+                        ) : null
+                    }
+                    <input
+                        type = "submit"
+                        className = "bg-gray-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-gray-900"
+                        value = "Guardar Cambios"
+                    >
+                    </input>
+                    </form>
+                </div>
+            </div>
+
+        </Layout>
+    );
+}
+
+export default EditarProducto;
